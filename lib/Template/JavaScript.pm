24ebb574697cfe71a2a6d9cc53e0c9a7930a3035
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-05-05 23:22:14 +0000

    re-arrange attributes for public first

--- lib/Template/JavaScript.pm
+++ lib/Template/JavaScript.pm
@@ -27,6 +27,23 @@ has bind => (
     documentation => 'Things to bind',
 );
 
+has template => (
+    is            => 'rw',
+    isa           => 'Str',
+    documentation => 'Things to bind',
+);
+
+has include_path => (
+    is            => 'rw',
+    isa           => 'Str|ArrayRef',
+    documentation => 'The include path for the templates',
+);
+
+has output => (
+    is            => 'rw',
+    isa           => 'Any',
+);
+
 has _context => (
     is            => 'ro',
     isa           => 'JavaScript::V8::Context',
@@ -44,23 +61,11 @@ has _result => (
     documentation => 'Result accumulator',
 );
 
-has template => (
-    is            => 'rw',
-    isa           => 'Str',
-    documentation => 'Things to bind',
-);
-
 has _tt => (
     is            => 'rw',
     isa           => 'Template',
     lazy_build    => 1,
     documentation => 'Our Template Toolkit object',
-);
-
-has include_path => (
-    is            => 'rw',
-    isa           => 'Str|ArrayRef',
-    documentation => 'The include path for the templates',
 );
 
 sub _build__tt {
@@ -76,11 +81,6 @@ sub _build__tt {
 
     return $tt;
 }
-
-has output => (
-    is            => 'rw',
-    isa           => 'Any',
-);
 
 sub BUILD {
     my ($self) = @_;
